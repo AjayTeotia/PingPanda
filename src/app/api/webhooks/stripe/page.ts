d2b0ab018c933c62b581f5e77b12ebc12f4273db
Ajay Teotia <teotiaajay20@gmail.com>
2024-11-07 01:30:09 +0530

    twelve-commit

--- src/app/api/webhooks/stripe/page.ts
+++ src/app/api/webhooks/stripe/page.ts
@@ -1,45 +1,32 @@
-// src/app/api/webhooks/stripe/route.ts
-
 import { db } from "@/db";
-import { stripe } from "@/lib/sripe";
+import { stripe } from "@/lib/sripe"; 
 import { headers } from "next/headers";
 import Stripe from "stripe";
 
 export async function POST(req: Request) {
-  // Read the request body and signature from headers
   const body = await req.text();
   const signature = headers().get("stripe-signature");
 
-  try {
-    // Construct the Stripe event
-    const event = stripe.webhooks.constructEvent(
-      body,
-      signature ?? "",
-      process.env.STRIPE_WEBHOOK_SECRET ?? ""
-    );
+  const event = stripe.webhooks.constructEvent(
+    body,
+    signature ?? "",
+    process.env.STRIPE_WEBHOOK_SECRET ?? ""
+  );
 
-    // Handle the checkout.session.completed event
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object as Stripe.Checkout.Session;
+  if (event.type === "checkout.session.completed") {
+    const session = event.data.object as Stripe.Checkout.Session;
 
-      const { userId } = session.metadata || { userId: null };
+    const { userId } = session.metadata || { userId: null };
 
-      // Validate metadata
-      if (!userId) {
-        return new Response("Invalid metadata", { status: 400 });
-      }
-
-      // Update user data in the database
-      await db.user.update({
-        where: { id: userId },
-        data: { plan: "PRO" },
-      });
+    if (!userId) {
+      return new Response("Invalid metadata", { status: 400 });
     }
 
-    // Return a success response
-    return new Response("OK");
-  } catch (error) {
-    console.error("Error processing webhook:", error);
-    return new Response("Webhook Error: " + error.message, { status: 400 });
+    await db.user.update({
+      where: { id: userId },
+      data: { plan: "PRO" },
+    });
   }
+
+  return new Response("OK");
 }
