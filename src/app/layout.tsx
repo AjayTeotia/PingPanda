8c9c64d9481ce0606bd7646cdcc0f223353138d1
Ajay Teotia <163528969+AjayTeotia@users.noreply.github.com>
2024-11-09 12:19:11 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,9 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en" className={cn(inter.variable, eb_garamond.variable)}
-        <meta name="google-site-verification" content="PlyNTnRmeeRRrtj6v-qiBEeL-VTAutJNe9i-SDkvhRI" />
-      >
+      <html lang="en" className={cn(inter.variable, eb_garamond.variable)}>
         <body className="min-h-[calc(100vh-1px)] flex flex-col font-sans bg-brand-50 text-brand-950 antialiased">
           <main className="relative flex-1 flex flex-col">
             <Providers>{children}
